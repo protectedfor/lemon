8f979acdf09a8424f4a773fa368a41d5aa959276
Andrey Fedotov <protected.for@gmail.com>
2021-10-09 13:52:07 +0600

    added laravel-voyager + customized pagination for server render

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return [
-        'api'     => 'lemon',
-        'version' => '1.0.0'
-    ];
-});
+Voyager::routes();
