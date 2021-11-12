909ed52c101fe13ca4c65700674ae04f932b9e51
Andrey Fedotov <protected.for@gmail.com>
2021-11-12 16:21:46 +0600

    added policies to provider

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,9 +2,18 @@
 
 namespace App\Providers;
 
+use App\Models\Invoice;
+use App\Models\Organization;
 use App\Models\Partner;
+use App\Models\Report;
+use App\Models\Transaction;
+use App\Policies\InvoicePolicy;
+use App\Policies\OrganizationPolicy;
 use App\Policies\PartnerPolicy;
+use App\Policies\ReportPolicy;
+use App\Policies\TransactionPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -14,7 +23,11 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-
+        Organization::class => OrganizationPolicy::class,
+        Partner::class      => PartnerPolicy::class,
+        Invoice::class      => InvoicePolicy::class,
+        Report::class       => ReportPolicy::class,
+        Transaction::class  => TransactionPolicy::class,
     ];
 
     /**
@@ -27,5 +40,29 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         //
+    }
+
+    /**
+     * Register the application's policies.
+     *
+     * @return void
+     */
+    public function registerPolicies()
+    {
+        if(!request()->isJson())
+            $this->policies = [];
+        foreach ($this->policies() as $key => $value) {
+            Gate::policy($key, $value);
+        }
+    }
+
+    /**
+     * Get the policies defined on the provider.
+     *
+     * @return array
+     */
+    public function policies()
+    {
+        return $this->policies;
     }
 }
